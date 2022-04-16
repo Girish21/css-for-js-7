9e64008bb83b6831afcb0a21f41bedd8238fdbc6
Girish21 <girishkumarv21@gmail.com>
2022-04-16 20:28:55 +0530

    first three tasks

---
+++ src/components/Header/Header.js
@@ -0,0 +1,128 @@
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
+
+import { QUERIES } from "../../constants";
+
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <SuperHeader>
+        <Row>
+          <ActionGroup>
+            <button>
+              <Search size={24} />
+            </button>
+            <button>
+              <Menu size={24} />
+            </button>
+          </ActionGroup>
+          <ResponsiveHeader>
+            <Logo />
+          </ResponsiveHeader>
+          <ActionGroupSmall>
+            <button>
+              <User size={24} />
+            </button>
+          </ActionGroupSmall>
+          <ActionGroupLarge>
+            <Button>Subscribe</Button>
+            <Link>Alread a subscriber?</Link>
+          </ActionGroupLarge>
+        </Row>
+      </SuperHeader>
+      <MainHeader>
+        <Logo />
+      </MainHeader>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.header`
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    margin-bottom: 80px;
+  }
+`;
+
+const SuperHeader = styled.div`
+  padding: 16px 0;
+  background: var(--color-gray-900);
+  color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    background: unset;
+    color: var(--color-gray-900);
+    flex: 1;
+  }
+`;
+
+const Row = styled(MaxWidthWrapper)`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ActionGroup = styled.div`
+  display: flex;
+  gap: 24px;
+
+  /*
+    FIX: Remove the inline spacing that comes with
+    react-feather icons.
+  */
+  svg {
+    display: block;
+  }
+`;
+
+const ActionGroupSmall = styled(ActionGroup)`
+  display: flex;
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`;
+
+const ActionGroupLarge = styled(ActionGroup)`
+  display: none;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 8px;
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`;
+
+const Link = styled.button`
+  font-style: italic;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+  text-decoration-line: underline;
+  color: var(--gray-900);
+`;
+
+const MainHeader = styled(MaxWidthWrapper)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 32px;
+  margin-bottom: 48px;
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`;
+
+const ResponsiveHeader = styled(MainHeader)`
+  display: none;
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    margin: 0;
+  }
+`;
+
+export default Header;
---
+++ src/components/MainStoryGrid/MainStoryGrid.js
@@ -0,0 +1,98 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+import { MAIN_STORY, OPINION_STORIES, SECONDARY_STORIES } from "../../data";
+
+import SectionTitle from "../SectionTitle";
+import MainStory from "../MainStory";
+import SecondaryStory from "../SecondaryStory";
+import OpinionStory from "../OpinionStory";
+import Advertisement from "../Advertisement";
+import { COLORS, QUERIES } from "../../constants";
+
+const MainStoryGrid = () => {
+  return (
+    <Wrapper>
+      <MainStorySection>
+        <MainStory {...MAIN_STORY} />
+      </MainStorySection>
+
+      <SecondaryStorySection>
+        <StoryList>
+          {SECONDARY_STORIES.map((story, index) => (
+            <SecondaryStory key={story.id} {...story} />
+          ))}
+        </StoryList>
+      </SecondaryStorySection>
+
+      <OpinionSection>
+        <SectionTitle>Opinion</SectionTitle>
+        <StoryList opinionSection>
+          {OPINION_STORIES.map((story, index) => (
+            <OpinionStory key={story.id} {...story} />
+          ))}
+        </StoryList>
+      </OpinionSection>
+
+      <AdvertisementSection>
+        <Advertisement />
+      </AdvertisementSection>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-areas:
+    "main-story"
+    "secondary-stories"
+    "opinion-stories"
+    "advertisement";
+  gap: 48px;
+  margin-bottom: 48px;
+`;
+
+const MainStorySection = styled.section`
+  grid-area: main-story;
+`;
+
+const SecondaryStorySection = styled.section`
+  grid-area: secondary-stories;
+`;
+
+const StoryList = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  & > * {
+    position: relative;
+  }
+
+  & > *:not([hidden]) + *:not([hidden]) {
+    margin-top: 32px;
+
+    &::before {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 1px;
+      background-color: ${COLORS.gray[300]};
+      left: 0;
+      right: 0;
+      top: -16px;
+      @media ${QUERIES.tabletOnly} {
+        content: ${({ opinionSection }) => (opinionSection ? "none" : "")};
+      }
+    }
+  }
+`;
+
+const OpinionSection = styled.section`
+  grid-area: opinion-stories;
+`;
+
+const AdvertisementSection = styled.section`
+  grid-area: advertisement;
+`;
+
+export default MainStoryGrid;
